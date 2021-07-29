83f9679ac818187a4b279f0f7837d0b6ec9c8eb6
vilaplanaangel <vilaplanaangel@gmail.com>
2021-07-30 01:36:36 +0200

    Add Update & Delete Logic CRM Claims

--- models/crm_claim.py
+++ models/crm_claim.py
@@ -14,17 +14,28 @@ class CrmClaim(models.Model):
             model = model_ref_id[0]
             model_value_id = model_ref_id[1]
             if model == 'sale.order':
-                sale_order = self.env['sale.order'].search([('id', '=', model_value_id)])
-                claims = sale_order.claims_ids
-                sale_order.write({'claims_ids': claims + claim})
+                self.add_claim_sale_order(claim, model_value_id)
         return claim
 
     def write(self, vals):
-        res = super(CrmClaim, self).write(vals)
-        return res
-        raise Exception('Write')
+        sale_order = self.model_ref_id
+        claim = super(CrmClaim, self).write(vals)
+        model_ref_id = vals.get('model_ref_id')
+        if model_ref_id:
+            model_ref_id = vals.get('model_ref_id').split(',')
+            model = model_ref_id[0]
+            model_value_id = model_ref_id[1]
+            if model == 'sale.order':
+                self.remove_claim_sale_order(sale_order)
+                self.add_claim_sale_order(self, model_value_id)
+        return claim
 
-    def unlink(self):
-        res = super(CrmClaim, self).unlink()
-        return res
-        raise Exception('Unlink')
+    def remove_claim_sale_order(self, sale_order):
+        claims = sale_order.claims_ids
+        if self in claims:
+            sale_order.write({'claims_ids': [(3, self.id, 0)]})
+
+    def add_claim_sale_order(self, claim, sale_order_id):
+        sale_order = self.env['sale.order'].search([('id', '=', sale_order_id)])
+        claims = sale_order.claims_ids
+        sale_order.write({'claims_ids': claims + claim})
--- models/sale_order.py
+++ models/sale_order.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    claims_ids = fields.Many2many('crm.claim', 'id', string='Claims')
+    claims_ids = fields.Many2many('crm.claim', 'id', string='Claims', ondelete="cascade")
